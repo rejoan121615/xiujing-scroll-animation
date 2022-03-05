f59fc48027dc125673fee033b26e836cff33ee25
Mohd Rejoan <wearecrx@gmail.com>
2022-03-05 12:29:10 +0600

    control, illuminate animation complete for xxl screen

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 gsap.registerPlugin(ScrollTrigger);
 
 // visual timeline
-const visualTimeline = gsap.timeline();
+const masterTl = gsap.timeline();
 
 function visualImgOneXXl() {
     const tl = gsap.timeline();
@@ -48,7 +48,6 @@ function visualImgOneXXl() {
         animation: tl,
         trigger: "#visualize-one",
         endTrigger: "#visualize-one",
-        markers: true,
         start: "51% center",
         // end: "100% 0%",
         scrub: true,
@@ -101,19 +100,158 @@ function visualImgOneXl() {
         animation: tl,
         trigger: "#visualize-one",
         endTrigger: "#visualize-one",
-        markers: true,
         start: "51% center",
         // end: "100% 0%",
         scrub: true,
         pin: "#visualize-one .bottom-sec .img-grid",
     });
 }
+// control timeline ----------------------------------------
+
+function controlXXl() {
+    const tl = gsap.timeline();
+    tl.fromTo(
+        "#control-one .img-grid .round",
+        {
+            x: "0",
+            y: "0",
+        },
+        {
+            x: "-580px",
+            y: "250px",
+            scale: "1.2",
+            duration: 1,
+        }
+    )
+        .fromTo(
+            "#control-one .img-grid .form",
+            {
+                x: "0",
+                y: "0",
+            },
+            {
+                x: "-80px",
+                y: "115px",
+                scale: "0.9",
+                duration: 1,
+            },
+            "<"
+        )
+        .fromTo(
+            "#control-one .img-grid .sq-one",
+            {
+                x: "0",
+                y: "0",
+            },
+            {
+                x: "-286px",
+                y: "-128px",
+                scale: "0.9",
+                duration: 1,
+            },
+            "<"
+        )
+        .fromTo(
+            "#control-one .img-grid .sq-two",
+            {
+                x: "0",
+                y: "0",
+            },
+            {
+                x: "-503px",
+                y: "-306px",
+                scale: "0.9",
+                duration: 1,
+            },
+            "<"
+        )
+        .fromTo(
+            "#control-one .img-grid .sq-three",
+            {
+                x: "0",
+                y: "0",
+            },
+            {
+                x: "-392px",
+                y: "-367px",
+                scale: "0.9",
+                duration: 1,
+            },
+            "<"
+        );
+
+    ScrollTrigger.create({
+        animation: tl,
+        trigger: "#control-one",
+        endTrigger: "#control-one",
+        start: "51% center",
+        // end: "100% 0%",
+        scrub: true,
+        pin: "#control-one .bottom-sec .img-grid",
+    });
+}
+
+// illuminate timeline ----------------------------------------
+
+function illuminateXXl() {
+    const tl = gsap.timeline();
+    tl.fromTo(
+        "#illuminate-one .img-grid .tyers",
+        {
+            x: "0",
+            y: "0",
+        },
+        {
+            y: "-70px",
+            x: "-22px",
+            scale: "0.95",
+            duration: 1,
+        }
+    )
+        .fromTo(
+            "#illuminate-one .img-grid .blur-img",
+            {
+                x: "0",
+                y: "0",
+            },
+            {
+                y: "-155px",
+                x: "-418px",
+                scale: "0.58",
+                duration: 1,
+            }, '<'
+        )
+        .fromTo(
+            "#illuminate-one .img-grid .cards",
+            {
+                x: "0",
+                y: "0",
+            },
+            {
+                y: "8px",
+                x: "-997px",
+                scale: "1",
+                duration: 1,
+            }, "<"
+        );
+
+    ScrollTrigger.create({
+        animation: tl,
+        trigger: "#illuminate-one",
+        endTrigger: "#illuminate-one",
+        markers: true,
+        start: "51% center",
+        // end: "100% 0%",
+        scrub: true,
+        pin: "#illuminate-one .bottom-sec .img-grid",
+    });
+}
 
 ScrollTrigger.matchMedia({
     "(min-width: 1200px)": function () {
-        visualImgOneXl();
+        masterTl.add(visualImgOneXl());
     },
     "(min-width: 1400px)": function () {
-        visualTimeline.add(visualImgOneXXl());
+        masterTl.add(visualImgOneXXl()).add(controlXXl()).add(illuminateXXl());
     },
 });
