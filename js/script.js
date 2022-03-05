ceaa4d119b07dd205d0e0aa00ff7943f67803a1c
Mohd Rejoan <wearecrx@gmail.com>
2022-03-05 13:26:40 +0600

    illution, control xl screen animation complete

--- js/script.js
+++ js/script.js
@@ -190,6 +190,89 @@ function controlXXl() {
         pin: "#control-one .bottom-sec .img-grid",
     });
 }
+function controlXl() {
+    const tl = gsap.timeline();
+    tl.fromTo(
+        "#control-one .img-grid .round",
+        {
+            x: "0",
+            y: "0",
+        },
+        {
+            x: "-500px",
+            y: "250px",
+            scale: "1.2",
+            duration: 1,
+        }
+    )
+        .fromTo(
+            "#control-one .img-grid .form",
+            {
+                x: "0",
+                y: "0",
+            },
+            {
+                x: "-100px",
+                y: "155px",
+                scale: "0.9",
+                duration: 1,
+            },
+            "<"
+        )
+        .fromTo(
+            "#control-one .img-grid .sq-one",
+            {
+                x: "0",
+                y: "0",
+            },
+            {
+                x: "-250px",
+                y: "-122px",
+                scale: "1.1",
+                duration: 1,
+            },
+            "<"
+        )
+        .fromTo(
+            "#control-one .img-grid .sq-two",
+            {
+                x: "0",
+                y: "0",
+            },
+            {
+                x: "-433px",
+                y: "-300px",
+                scale: "1.05",
+                duration: 1,
+            },
+            "<"
+        )
+        .fromTo(
+            "#control-one .img-grid .sq-three",
+            {
+                x: "0",
+                y: "0",
+            },
+            {
+                x: "-342px",
+                y: "-360px",
+                scale: "1.05",
+                duration: 1,
+            },
+            "<"
+        );
+
+    ScrollTrigger.create({
+        animation: tl,
+        trigger: "#control-one",
+        endTrigger: "#control-one",
+        start: "51% center",
+        // end: "100% 0%",
+        scrub: true,
+        pin: "#control-one .bottom-sec .img-grid",
+        markers: true,
+    });
+}
 
 // illuminate timeline ----------------------------------------
 
@@ -219,7 +302,8 @@ function illuminateXXl() {
                 x: "-418px",
                 scale: "0.58",
                 duration: 1,
-            }, '<'
+            },
+            "<"
         )
         .fromTo(
             "#illuminate-one .img-grid .cards",
@@ -232,7 +316,64 @@ function illuminateXXl() {
                 x: "-997px",
                 scale: "1",
                 duration: 1,
-            }, "<"
+            },
+            "<"
+        );
+
+    ScrollTrigger.create({
+        animation: tl,
+        trigger: "#illuminate-one",
+        endTrigger: "#illuminate-one",
+        markers: true,
+        start: "51% center",
+        // end: "100% 0%",
+        scrub: true,
+        pin: "#illuminate-one .bottom-sec .img-grid",
+    });
+}
+
+function illuminateXl() {
+    const tl = gsap.timeline();
+    tl.fromTo(
+        "#illuminate-one .img-grid .tyers",
+        {
+            x: "0",
+            y: "0",
+        },
+        {
+            y: "-70px",
+            x: "-22px",
+            scale: "0.95",
+            duration: 1,
+        }
+    )
+        .fromTo(
+            "#illuminate-one .img-grid .blur-img",
+            {
+                x: "0",
+                y: "0",
+            },
+            {
+                y: "-111px",
+                x: "-390px",
+                scale: "0.5",
+                duration: 1,
+            },
+            "<"
+        )
+        .fromTo(
+            "#illuminate-one .img-grid .cards",
+            {
+                x: "0",
+                y: "0",
+            },
+            {
+                y: "35px",
+                x: "-860px",
+                scale: "1.02",
+                duration: 1,
+            },
+            "<"
         );
 
     ScrollTrigger.create({
@@ -249,7 +390,7 @@ function illuminateXXl() {
 
 ScrollTrigger.matchMedia({
     "(min-width: 1200px)": function () {
-        masterTl.add(visualImgOneXl());
+        masterTl.add(visualImgOneXl()).add(controlXl()).add(illuminateXl());
     },
     "(min-width: 1400px)": function () {
         masterTl.add(visualImgOneXXl()).add(controlXXl()).add(illuminateXXl());
