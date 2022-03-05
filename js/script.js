16eeeb889575e5516344a3d021c138d24c3f351e
Mohd Rejoan <wearecrx@gmail.com>
2022-03-05 11:24:40 +0600

    xl animation complete for visuals

--- js/script.js
+++ js/script.js
@@ -3,17 +3,7 @@ gsap.registerPlugin(ScrollTrigger);
 // visual timeline
 const visualTimeline = gsap.timeline();
 
-// visual scroll trigger
-// ScrollTrigger.create({
-//     animation: visualTimeline,
-//     trigger: "#visualize-one",
-//     markers: true,
-//     start: "51% center",
-//     end: "100% 0%",
-//     scrub: true,
-// });
-
-function visualImgOne() {
+function visualImgOneXXl() {
     const tl = gsap.timeline();
     tl.fromTo(
         "#visualize-one .img-grid .one",
@@ -27,30 +17,32 @@ function visualImgOne() {
             duration: 1,
         }
     )
-    .fromTo(
-        "#visualize-one .img-grid .two",
-        {
-            x: "0",
-            y: "0",
-        },
-        {
-            x: "-660px",
-            y: "-105px",
-            duration: 1,
-        }, '<'
-    )
-    .fromTo(
-        "#visualize-one .img-grid .three",
-        {
-            x: "0",
-            y: "0",
-        },
-        {
-            x: "-108px",
-            y: "-275px",
-            duration: 1,
-        }, "<"
-    )
+        .fromTo(
+            "#visualize-one .img-grid .two",
+            {
+                x: "0",
+                y: "0",
+            },
+            {
+                x: "-660px",
+                y: "-105px",
+                duration: 1,
+            },
+            "<"
+        )
+        .fromTo(
+            "#visualize-one .img-grid .three",
+            {
+                x: "0",
+                y: "0",
+            },
+            {
+                x: "-108px",
+                y: "-275px",
+                duration: 1,
+            },
+            "<"
+        );
 
     ScrollTrigger.create({
         animation: tl,
@@ -64,4 +56,64 @@ function visualImgOne() {
     });
 }
 
-visualTimeline.add(visualImgOne());
+function visualImgOneXl() {
+    const tl = gsap.timeline();
+    tl.fromTo(
+        "#visualize-one .img-grid .one",
+        {
+            x: "0",
+            y: "0",
+        },
+        {
+            x: "-508px",
+            y: "-105px",
+            duration: 1,
+        }
+    )
+        .fromTo(
+            "#visualize-one .img-grid .two",
+            {
+                x: "0",
+                y: "0",
+            },
+            {
+                x: "-560px",
+                y: "-80px",
+                duration: 1,
+            },
+            "<"
+        )
+        .fromTo(
+            "#visualize-one .img-grid .three",
+            {
+                x: "0",
+                y: "0",
+            },
+            {
+                x: "-88px",
+                y: "-274px",
+                duration: 1,
+            },
+            "<"
+        );
+
+    ScrollTrigger.create({
+        animation: tl,
+        trigger: "#visualize-one",
+        endTrigger: "#visualize-one",
+        markers: true,
+        start: "51% center",
+        // end: "100% 0%",
+        scrub: true,
+        pin: "#visualize-one .bottom-sec .img-grid",
+    });
+}
+
+ScrollTrigger.matchMedia({
+    "(min-width: 1200px)": function () {
+        visualImgOneXl();
+    },
+    "(min-width: 1400px)": function () {
+        visualTimeline.add(visualImgOneXXl());
+    },
+});
