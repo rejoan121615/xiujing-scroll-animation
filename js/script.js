1bb9499e965be3e488adeeedc1acab74befde146
Mohd Rejoan <wearecrx@gmail.com>
2022-03-05 10:51:15 +0600

    visualize section complete

--- js/script.js
+++ js/script.js
@@ -1,3 +1,67 @@
-'use strict';
+gsap.registerPlugin(ScrollTrigger);
 
-console.log(gsap)
+// visual timeline
+const visualTimeline = gsap.timeline();
+
+// visual scroll trigger
+// ScrollTrigger.create({
+//     animation: visualTimeline,
+//     trigger: "#visualize-one",
+//     markers: true,
+//     start: "51% center",
+//     end: "100% 0%",
+//     scrub: true,
+// });
+
+function visualImgOne() {
+    const tl = gsap.timeline();
+    tl.fromTo(
+        "#visualize-one .img-grid .one",
+        {
+            x: "0",
+            y: "0",
+        },
+        {
+            x: "-598px",
+            y: "-88px",
+            duration: 1,
+        }
+    )
+    .fromTo(
+        "#visualize-one .img-grid .two",
+        {
+            x: "0",
+            y: "0",
+        },
+        {
+            x: "-660px",
+            y: "-105px",
+            duration: 1,
+        }, '<'
+    )
+    .fromTo(
+        "#visualize-one .img-grid .three",
+        {
+            x: "0",
+            y: "0",
+        },
+        {
+            x: "-108px",
+            y: "-275px",
+            duration: 1,
+        }, "<"
+    )
+
+    ScrollTrigger.create({
+        animation: tl,
+        trigger: "#visualize-one",
+        endTrigger: "#visualize-one",
+        markers: true,
+        start: "51% center",
+        // end: "100% 0%",
+        scrub: true,
+        pin: "#visualize-one .bottom-sec .img-grid",
+    });
+}
+
+visualTimeline.add(visualImgOne());
